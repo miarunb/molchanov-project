80e389f11fdfe424c5d4311742ab237d2c5cd7f8
miarunb <miarunb@yandex.ru>
2021-06-11 06:04:26 +0600

    Replacing functions with classes/ Adding mixins

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 
 from .models import Post, Tag
+from .utils import ObjectDetailMixin
 
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={"posts": posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+#def post_detail(request, slug):
+    #post = Post.objects.get(slug__iexact=slug)
+    #return render(request, 'blog/post_detail.html', context={'post': post})
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+    #def get(self, request, slug):
+        ##post = Post.objects.get(slug__iexact=slug)
+        #post = get_object_or_404(Post, slug__iexact=slug)
+        #return render(request, 'blog/post_detail.html', context={'post': post})
+
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+#def tag_detail(request, slug):
+    #tag = Tag.objects.get(slug__iexact=slug)
+    #return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
